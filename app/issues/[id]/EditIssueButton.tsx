ea168e8079eaea715fec13471a69f9b0fb37a5ef
neverlose-lv <volkk@inbox.lv>
2024-01-25 11:54:17 +0200

    fixed link layout

--- app/issues/[id]/EditIssueButton.tsx
+++ app/issues/[id]/EditIssueButton.tsx
@@ -5,12 +5,12 @@ import React from 'react';
 
 const EditIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button>
-      <Link href={`/issues/edit/${issueId.toString()}`}>
+    <Link href={`/issues/edit/${issueId.toString()}`}>
+      <Button>
         <Pencil2Icon className="inline mr-1 align-middle size-5 pb-1 -ml-1" />
         Edit Issue
-      </Link>
-    </Button>
+      </Button>
+    </Link>
   );
 };
 
